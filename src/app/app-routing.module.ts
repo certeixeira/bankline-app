786ebd3f648eb6f02f31a13b077a2ad51a72b51b
Carlos Eduardo Rangel Teixeira <certeixeira@outlook.com>
2022-05-04 17:36:15 -0300

    projeto concluído

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MovimentacaoNewComponent } from './components/movimentacao-new/movimentacao-new.component';
 import { MovimentacaoListComponent } from './components/movimentacao-list/movimentacao-list.component';
+import { CorrentistaComponent } from './components/correntista/correntista.component';
 
 const routes: Routes = [
+  { path: 'correntistas', component: CorrentistaComponent },
   { path: 'movimentacoes-new', component: MovimentacaoNewComponent },
   { path: 'movimentacoes', component: MovimentacaoListComponent },
   { path: '', redirectTo: 'movimentacoes', pathMatch: 'full' },
